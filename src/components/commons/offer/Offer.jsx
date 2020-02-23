81461fe4d2aeca817915104173fbd321af865c0a
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2020-02-23 01:12:34 +0100

    offfer section created

---
+++ src/components/commons/offer/Offer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import "./Offer.css";
+
+export default function Offer() {
+  return (
+    <div className="Offer">
+      Google offered in:
+      <ul>
+        <li>
+          <a href="#"> Hausa</a>
+        </li>
+        <li>
+          <a href="#">Igbo</a>
+        </li>
+        <li>
+          <a href="#">Èdè Yorùbá</a>
+        </li>
+        <li>
+          <a href="#">Nigerian Pidgin</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
